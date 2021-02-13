6ace43275c3fdfd73828a60c243d6422f6d9b9df
Sergey Buza <buza@gmail.com>
2021-02-13 14:42:08 +0300

    написал функцию compact

---
+++ compact.js
@@ -0,0 +1,15 @@
+compact = (arr) => {
+  /*
+    Создает массив со всеми удаленными значениями false.
+    Значения false, null, 0, "", undefined и NaN являются ложными.
+*/
+  const result = [];
+  arr.forEach((item) => {
+    item && result.push(item);
+  });
+  return result;
+};
+/*
+compact([0, 1, false, 2, "", 3, NaN, null, undefined]);
+// => [1, 2, 3] 
+*/
