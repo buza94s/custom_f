ba4f9dcac7959ba992416abc32e5f98e7be19740
Sergey Buza <buza@gmail.com>
2021-02-13 14:29:42 +0300

    добавлена функциия chunk

---
+++ chunk.js
@@ -0,0 +1,22 @@
+const chunk = (arr, n) => {
+  /*Создает массив элементов, разбитых на группы по длине размера.
+    Если массив не может быть разделен равномерно, то последним фрагментом будут оставшиеся элементы.*/
+  const result = [];
+  const newArr = [...arr];
+  const f = (arr) => {
+    if (arr.length < n) {
+      return result.push(arr);
+    }
+    result.push(arr.splice(0, n));
+    return arr.length > 0 && f(arr);
+  };
+  f(newArr);
+  return result;
+};
+/*
+  chunk(['a', 'b', 'c', 'd'], 2);
+  => [['a', 'b'], ['c', 'd']]
+ 
+  chunk(['a', 'b', 'c', 'd'], 3);
+  => [['a', 'b', 'c'], ['d']]
+*/
